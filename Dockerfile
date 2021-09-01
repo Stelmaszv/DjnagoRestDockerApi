1fe153a401ef7a8b8efab47d85709ab942f5dd93
stelmaszv <stelmaszv@gmail.com>
2021-09-01 16:47:58 +0200

    Updata class SetUp for herocu (setup.py)

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,11 @@ WORKDIR /app
 
 ADD . .
 
-EXPOSE 8000
+#for localhost
+#EXPOSE 8000
 
-CMD ["gunicorn", "--bind", ":8000", "--workers", "3", "core.wsgi:application"]
+#CMD ["gunicorn", "--bind", ":8000", "--workers", "3", "core.wsgi:application"]
+
+
+#for local heroku
+CMD gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
--- core/settings/setup.py
+++ core/settings/setup.py
@@ -5,10 +5,15 @@ load_dotenv()
 class SetUp:
     debug = int(os.getenv('DEBUG'))
     secret_key = os.getenv('SECRET_KEY')
+    heroku = os.getenv('heroku')
 
     def set_allowed_host(self):
         host=[]
-        if self.debug == 0:
-            host.append(os.getenv('HOST1'))
-            host.append(os.getenv('HOST2'))
+        if self.heroku==0:
+            if self.debug == 0:
+                host.append(os.getenv('HOST1'))
+                host.append(os.getenv('HOST2'))
+        else:
+            if self.debug == 1:
+                host.append(os.getenv('HOST3'))
         return host
