f912b9def92d837abd49180342448d4b362a5b71
stelmaszv <stelmaszv@gmail.com>
2021-09-03 12:48:08 +0200

    Fix error in models "Rate" and "Car" (cars/models.py)

--- apps/cars/models.py
+++ apps/cars/models.py
@@ -5,18 +5,15 @@ class Rate(models.Model):
     id = models.AutoField(primary_key=True)
     rating     = models.IntegerField(null=True)
     car_id     = models.ForeignKey(
-        to='netgurutask.Car',
+        to='cars.Car',
         on_delete=models.CASCADE,
         related_name='car',null=True
     )
 
 class Car(models.Model):
-    id             = models.AutoField(primary_key=True)
-    make           = models.CharField(max_length=100)
-    model          = models.CharField(max_length=100)
-    rates_number   = models.IntegerField(default=1,null=False)
-    avg_rating     = models.FloatField(default=1)
-    rates          = models.ManyToManyField(
-        to='netgurutask.Rate',
-        related_name='rates'
-    )
+    id           = models.AutoField(primary_key=True)
+    make         = models.CharField(max_length=100)
+    model        = models.CharField(max_length=100)
+    rates_number = models.IntegerField(default=1,null=False)
+    avg_rating   = models.FloatField(default=1)
+    rates = models.ManyToManyField(to='cars.Rate', related_name='rates')
