a2fd039926d755693e69db56b0df614662d4ac92
stelmaszv <stelmaszv@gmail.com>
2021-09-03 12:53:12 +0200

    Add view in views "APIPrototype" (cars/views.py)

--- apps/cars/views.py
+++ apps/cars/views.py
@@ -11,3 +11,31 @@ class APIPrototypeGet(APIView):
     def api_get(self, request, *args, **kwargs):
         serializer = self.serializer_class(self.queryset)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+class APIPrototype(APIView):
+
+    serializer_class = None
+    many     = True
+    queryset = ''
+    order_by = ''
+
+    def list(self):
+        serializer = self.serializer_class(self.queryset, many=self.many)
+        if len(self.order_by):
+            list = sorted(serializer.data, key=lambda tup: tup[self.order_by],reverse=True)
+        else:
+            list= serializer.data
+        return list
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,many=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=self.list(), status=status.HTTP_201_CREATED)
+        return self.api_get(request)
+
+    def api_get(self, request, *args, **kwargs):
+        return Response(data=self.list(), status=status.HTTP_200_OK)
+
+    def get(self, request, *args, **kwargs):
+        return self.api_get(request)
