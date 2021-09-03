c737372b59a9a8058ea2a127d29c60838d123126
stelmaszv <stelmaszv@gmail.com>
2021-09-03 13:50:39 +0200

    Update clean code in views

--- apps/cars/views.py
+++ apps/cars/views.py
@@ -1,28 +1,29 @@
+from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import  APIView
 from .models import Car,Rate
 from .serializers import CarSerializer,CarSerializerPopular,RateSerializer,CarDeleteSerializer
 class APIPrototypeGet(APIView):
-    serializer_class=None
+    SerializerClass=None
     queryset=None
 
     def get(self, request, *args, **kwargs):
         return self.api_get(request)
 
     def api_get(self, request, *args, **kwargs):
-        serializer = self.serializer_class(self.queryset)
+        serializer = self.SerializerClass(self.queryset)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 class APIPrototype(APIView):
 
-    serializer_class = None
+    SerializerClass = None
     many     = True
     queryset = ''
     order_by = ''
 
     def list(self):
-        serializer = self.serializer_class(self.queryset, many=self.many)
+        serializer = self.SerializerClass(self.queryset, many=self.many)
         if len(self.order_by):
             list = sorted(
                 serializer.data,
@@ -33,7 +34,7 @@ class APIPrototype(APIView):
         return list
 
     def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,many=False)
+        serializer = self.SerializerClass(data=request.data, many=False)
         if serializer.is_valid():
             serializer.save()
             return Response(data=self.list(), status=status.HTTP_201_CREATED)
@@ -48,7 +49,7 @@ class APIPrototype(APIView):
 class CarDelete(APIPrototypeGet):
 
     many             = False
-    serializer_class = CarDeleteSerializer
+    SerializerClass = CarDeleteSerializer
     queryset = Car.objects
 
     def get_object(self, pk):
@@ -59,22 +60,21 @@ class CarDelete(APIPrototypeGet):
             raise Http404
 
     def delete(self, request, *args, **kwargs):
-
         car = self.get_object(self.kwargs.get("id"))
         car.delete()
         mess =str(car)+' Has been removed from data base'
         return Response(data=mess, status=status.HTTP_200_OK)
 
 class CarList (APIPrototype):
-    serializer_class  = CarSerializer
+    SerializerClass  = CarSerializer
     queryset          = Car.objects
     order_by          = 'avg_rating'
 
 class CarListPupular (APIPrototype):
-    serializer_class = CarSerializerPopular
+    SerializerClass = CarSerializerPopular
     queryset         = Car.objects
     order_by         = 'rates_number'
 
 class AddRate(APIPrototype):
-    serializer_class = RateSerializer
+    SerializerClass = RateSerializer
     queryset=  Rate.objects
