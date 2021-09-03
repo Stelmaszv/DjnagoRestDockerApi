1df8da3e8839a1e13c9cc17f74e3c030039cf5ff
stelmaszv <stelmaszv@gmail.com>
2021-09-03 13:00:26 +0200

    Add view (CarList) to urls (cars/urls.py)

--- apps/cars/urls.py
+++ apps/cars/urls.py
@@ -1,2 +1,7 @@
+from django.urls import path
+from apps.cars.views import CarList
 app_name = 'cars'
-urlpatterns = []
+
+urlpatterns = [
+    path('carslist', CarList.as_view(),name='cars_list'),
+]
