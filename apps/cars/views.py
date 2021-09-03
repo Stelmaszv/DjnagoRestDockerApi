1b1768f9d7b41d7c82bf9b8cdbf3926f5ce24701
stelmaszv <stelmaszv@gmail.com>
2021-09-03 12:51:53 +0200

    Add view in views "APIPrototypeGet" (cars/views.py)

--- apps/cars/views.py
+++ apps/cars/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import  APIView
+class APIPrototypeGet(APIView):
+    serializer_class=None
+    queryset=None
 
-# Create your views here.
+    def get(self, request, *args, **kwargs):
+        return self.api_get(request)
+
+    def api_get(self, request, *args, **kwargs):
+        serializer = self.serializer_class(self.queryset)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
